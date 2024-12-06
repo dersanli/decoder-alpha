48ab6e740d075c6aefab52ea58f06c2421c658e0
Devrim Ersanli <dersanli@gmail.com>
2024-12-06 11:41:27 +0000

    Get the balance of wallet and display on the button caption

---
+++ src/util/queryChain.ts
@@ -0,0 +1,12 @@
+import * as ethers from 'ethers';
+import abi from './contract-abi.json';
+
+const rpcEndpoint = 'https://bsc-dataseed.bnbchain.org';
+const contractAddress = '0xb2ea51BAa12C461327d12A2069d47b30e680b69D';
+const walletAddress = '0x248Dd3836E2A8B56279C04addC2D11F3c2497836';
+
+const provider = new ethers.JsonRpcProvider(rpcEndpoint);
+const contract = new ethers.Contract(contractAddress, abi, provider);
+
+export const getBalanceOfContract = async () =>
+    (await contract.balanceOf(walletAddress)).toString();
